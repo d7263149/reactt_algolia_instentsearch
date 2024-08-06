2230b922ccc5d3ee3da400ae6f74b7ed0bf8b40f
Sandeep <sandeepsharma999000@gmail.com>
2024-08-06 19:05:24 +0530

    ss

---
+++ src/App.tsx
@@ -0,0 +1,77 @@
+import React from 'react';
+import algoliasearch from 'algoliasearch/lite';
+import {
+  Configure,
+  DynamicWidgets,
+  RefinementList,
+  Hits,
+  InstantSearch,
+  Pagination,
+  SearchBox,
+} from 'react-instantsearch';
+
+import { Panel } from './Panel';
+
+import type { Hit } from 'instantsearch.js';
+
+import './App.css';
+
+const searchClient = algoliasearch(
+  'QNMZQEBJD0',
+  '302790e6536a1f4f743b0a8cbcda84e4'
+);
+
+const future = { preserveSharedStateOnUnmount: true };
+
+export function App() {
+  return (
+    <div>
+      <header className="header">
+        <h1 className="header-title">
+          <a href="/">my-app</a>
+        </h1>
+        <p className="header-subtitle">
+          using{' '}
+          <a href="https://github.com/algolia/instantsearch/tree/master/packages/react-instantsearch">
+            React InstantSearch
+          </a>
+        </p>
+      </header>
+
+      <div className="container">
+        <InstantSearch searchClient={searchClient} indexName="products" future={future} insights>
+          <Configure hitsPerPage={8} />
+          <div className="search-panel">
+            <div className="search-panel__filters">
+              <DynamicWidgets fallbackComponent={RefinementList}>
+              </DynamicWidgets>
+            </div>
+
+            <div className="search-panel__results">
+              <SearchBox placeholder="" className="searchbox" />
+              <Hits hitComponent={Hit} />
+
+              <div className="pagination">
+                <Pagination />
+              </div>
+            </div>
+          </div>
+        </InstantSearch>
+      </div>
+    </div>
+  );
+}
+
+type HitProps = {
+  hit: Hit;
+};
+
+function Hit({ hit }: HitProps) {
+  return (
+    <article>
+      <p>
+        <code>{JSON.stringify(hit).slice(0, 100)}...</code>
+      </p>
+    </article>
+  );
+}
